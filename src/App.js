8f734fa535e02afeadd24677114f00fc410db765
Kendrick <gnfi@ool-ad0202bf.dyn.optonline.net>
2019-04-11 10:45:42 -0400

    added morning-review solution

--- src/App.js
+++ src/App.js
@@ -1,52 +1,87 @@
 import React, { Component } from 'react';
 import './App.css';
 
-/* 
- * Perhaps some hints?
- *
- * 1. Use a constructor and initialize state inside of it
- * 2. Set up a fetch function in fetchAllGifs(). **Use your giphy api endpoint as url**
- * 3. Call your fetch function inside of componentDidMount()
- * 4. Set up a loading message until promise is resolved, and fetch status: 200
- * 5. Maybe map through the fetched data and get them all to render on the page before you add the button functionality
- */
+import Axios from 'axios';
 
 class App extends Component {
-    // this.state = {
-    //   allGifs: [],
-    //   fetchOK: null
-    // }
+  constructor() {
+    super();
 
+    this.state = {
+      allGifs: null,
+      isLoaded: null
+    }
+    console.log('init', this.state.allGifs); // console log my initial state for debugging
+
+    // bind our API call functions to 'this' context.
+    // look back on es6 classes for 'this' context.
+    this.callGiphy.bind(this);
+    this.callGiphyV2.bind(this);
+  }
+
+  // componentDidMount() is a React Life Cycle Method (it's a function!)
+  // this react life cycle method gets priority once react app is rendered
   componentDidMount() {
-    // call fetch function here
+    // this.callGiphy();
   }
 
-  fetchAllGifs() {
-    // declare a url variable for the Giphy API endpoint
-    // RETURN all fetched gifs!
-    // set state with the data you've fetched from the API to allGifs
-    // and set fetchOK to true.
+  // 1st try: Use promises with fetch API.
+  callGiphy() {
+    let url = 'https://api.giphy.com/v1/gifs/random?api_key=lTTC7iqi7W2WJDhxz3EGbOil23uYmndh';
+
+    return fetch(url)
+    .then((response) => {
+      // console.log(response.json());
+      return response.json()
+    })
+    .then((data) => {
+      const dataObj = data.data.images.original.url;
+      this.setState({
+        allGifs: dataObj,
+        isLoaded: true
+      });
+      console.log('fetched', this.state.allGifs); // console log the state after it's set
+    })
+    .catch(err => console.log(err.message));
   }
 
-
-  /*
-   * This function below could help you out :)
-   */
-
-  // renderFetch() {
-  //   if (this.state.fetchOK) { return <h1>Done</h1> }
-  //   else { return <h1>Loading...</h1> }
-  // }
+  // 2nd try: Using Asnyc/Await with Axios
+  async callGiphyV2() {
+    try {
+      const getGifs = await Axios({
+        method: 'get',
+        url: 'https://api.giphy.com/v1/gifs/random?api_key=lTTC7iqi7W2WJDhxz3EGbOil23uYmndh'
+      });
+      const gifObj = getGifs.data.data.images.original.url;
+  
+      this.setState({
+        allGifs: gifObj,
+        isLoaded: true
+      })
+      console.log('set', this.state.allGifs);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 
   render() {
+    /* set up a ternary for conditionally rendering.
+     * the ternary is checking if this.state.isLoaded is true or false.
+     * render 'Nope' if promise is rejected or if API call wasn't successful.
+     * render <img> element when promise is resolved and state is set.
+     */
+    const checkFetch = this.state.isLoaded ? <img src={this.state.allGifs}></img> : <h1>Nope</h1>
+    
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React Giphy</h1>
+          <h1 className="App-title">Hi Jeopardy! You're great!</h1>
         </header>
         <main>
-          I'm going to show a collection of gifs!
+          { checkFetch }
         </main>
+        <button onClick={() => this.callGiphy()}>Using Promises</button>
+        <button onClick={() => this.callGiphyV2()}>Using Async/Await</button>
       </div>
     );
   }
